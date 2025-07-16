a0b3a46a4a79bc92f02f3896bf8c630a545d5e60
binalayjki <binalayjki@gmail.com>
2025-07-16 22:01:34 +0800

    refinement of the back button on applicant side registration

--- frontend/src/components/Home/A_RegistrationForm.vue
+++ frontend/src/components/Home/A_RegistrationForm.vue
@@ -10,24 +10,26 @@
           </p>
         </div>
         <div class="pt-10 pb-2">
-          <button
-            class="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl text-dark-slate"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15 18l-6-6 6-6"
-              />
-            </svg>
-          </button>
+                <router-link to="/loginform">
+                  <button
+                    class="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl text-dark-slate"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 18l-6-6 6-6"
+                      />
+                    </svg>
+                  </button>
+                </router-link>
         </div>
 
         <h2
