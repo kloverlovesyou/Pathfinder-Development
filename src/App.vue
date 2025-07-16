69da1c0a522027d2c5a1a4dd459bef1ced1037fd
binalayjki <binalayjki@gmail.com>
2025-07-16 13:38:59 +0800

    Updated login and routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
+import { useRoute } from "vue-router";
 import Header from "./components/Layout/Header.vue";
 import Footer from "./components/Layout/Footer.vue";
 
-import LoginForm from "./components/Home/LoginForm.vue";
+// Access the current route
+const route = useRoute();
 </script>
 
 <template>
-  <LoginForm></LoginForm>
+  <div class="min-h-screen flex flex-col justify-between">
+    <Header v-if="route.path !== '/loginform'" />
+
+    <main class="flex-grow">
+      <router-view />
+    </main>
+    <Footer v-if="route.path !== '/loginform'" />
+  </div>
 </template>
 
 <style scoped></style>
