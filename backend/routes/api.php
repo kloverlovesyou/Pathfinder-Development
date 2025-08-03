94432759f63445e14cb9a81df41d05ca4eb3416a
binalayjki <binalayjki@gmail.com>
2025-08-03 08:08:48 +0800

    Add login route to API for user authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicantController;
 
 Route::post('/applicants', [ApplicantController::class, 'store']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [ApplicantController::class, 'login']);
