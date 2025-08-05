704e3ed96ee28e7d09093e5a26ea60ae68b514b7
kloverlovesyou <kassparoy21@gmail.com>
2025-08-05 22:17:05 +0800

    profile page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute } from "vue-router";
-import Header from "./components/Layout/Header.2.vue";
+import Header from "./components/Layout/Header2.vue";
 import Homepage from "./components/Home/Homepage.vue";
 import Footer from "./components/Layout/Footer.vue";
 import TypeOfAccount from "./components/Home/TypeOfAccount.vue";
@@ -18,16 +18,8 @@ const route = useRoute();
         route.path !== '/typeofaccount' &&
         route.path !== '/a_registrationform' &&
         route.path !== '/o_registrationform'
-      "></Header>
-    <Homepage></Homepage>
-    <!--<Header
-      v-if="
-        route.path !== '/loginform' &&
-        route.path !== '/typeofaccount' &&
-        route.path !== '/a_registrationform' &&
-        route.path !== '/o_registrationform'
       "
-    />
+    ></Header>
     <main class="flex-grow">
       <router-view></router-view>
     </main>
@@ -38,7 +30,7 @@ const route = useRoute();
         route.path !== '/a_registrationform' &&
         route.path !== '/o_registrationform'
       "
-    />-->
+    ></Footer>
   </div>
 </template>
 
--- frontend/src/Router/routes.js
+++ frontend/src/Router/routes.js
@@ -10,7 +10,7 @@ const router = createRouter({
   routes: [
     {
       path: "/",
-      redirect: "/homepage",
+      redirect: "/loginform",
     },
     {
       path: "/homepage",
