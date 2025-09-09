c8a8b893cf61139c1b9bdc3a39214db518e8b26e
binalayjki <binalayjki@gmail.com>
2025-09-09 10:17:33 +0800

    fix: update signout action to redirect to login form

--- frontend/src/components/Organization/OrganizationHomepage.vue
+++ frontend/src/components/Organization/OrganizationHomepage.vue
@@ -71,7 +71,7 @@
         </div>
 
         <div class="spacer"></div> <!-- pushes signout down -->
-        <div class="icon signout" @click="signOut">
+        <div class="icon signout" @click="$router.push('/loginform')">
           <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="white"
             stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
