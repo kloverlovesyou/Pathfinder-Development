731d3c05ba4c43c79060ddcbd7b62cd40b8ee535
binalayjki <binalayjki@gmail.com>
2025-09-09 14:02:06 +0800

    refactor: simplify header visibility logic in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,35 +15,12 @@ const noHeaderRoutes = [
   "/OrgCareers",
   "/updateprofile"
 ];
-</script>
+</script>x
 
 <template>
   <div class="min-h-screen flex flex-col justify-between">
-    <!-- Header -->
-    <Header
-      v-if="
-        route.path !== '/loginform' &&
-        route.path !== '/typeofaccount' &&
-        route.path !== '/a_registrationform' &&
-        route.path !== '/o_registrationform' &&
-        route.path !== '/OrgTrainings' &&
-        route.path !== '/OrgCareers' &&
-        route.path == '/updateprofile'
-      "
-    />
-
-    <!-- Main content -->
-    <main
-      class="flex-grow"
-      v-if="
-        route.path !== '/homepage' &&
-        route.path !== '/trainingpage' &&
-        route.path !== '/careerpage' &&
-        route.path !== '/organizationpage' &&
-        route.path !== '/bookmarkpage' &&
-        route.path !== '/calendarpage'
-      "
-    >
+    <Header v-if="!noHeaderRoutes.includes(route.path)" />
+    <main class="flex-grow">
       <router-view></router-view>
     </main>
     <Footer />
