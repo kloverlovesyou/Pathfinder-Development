75677861b62e5f9d2bbfcfad74e70ca18a3729fc
binalayjki <binalayjki@gmail.com>
2025-11-03 23:34:46 +0800

    xx

--- backend/app/Http/Controllers/CareerBookmarkController.php
+++ backend/app/Http/Controllers/CareerBookmarkController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\CareerBookmark;
+use App\Models\Careerbookmark;
 
 class CareerBookmarkController extends Controller
 {
