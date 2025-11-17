4c994593ac0992904a8ec0ed0e3db925cb8290dc
binalayjki <binalayjki@gmail.com>
2025-11-17 23:55:36 +0800

    ss

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -38,10 +38,10 @@ Route::get('/trainings', [TrainingController::class, 'index']);
 
 // Careers with recommendations
 Route::get('/careers', [CareerRecommendationController::class, 'index']);
-Route::get('/careers/recommend/{careerID}', [CareerRecommendationController::class, 'recommendedCareers'])
+Route::get('/careers/recommend/{careerID}', [CareerRecommendationController::class, 'getCareerWithRecommendations'])
      ->name('careers.recommendation');
 Route::get('/careers/{careerID}/trainings', [CareerRecommendationController::class, 'recommendedTrainings']);
-Route::get('/careers/{id}/details', [CareerRecommendationController::class, 'getCareerWithRecommendations']);
+
 
 // CareerController
 Route::get('/careers/recommend/manual/{id}', [CareerController::class, 'recommend'])
