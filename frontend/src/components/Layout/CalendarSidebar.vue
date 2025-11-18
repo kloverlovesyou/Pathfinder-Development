6926b240e5569ef2357fa33f1ee28ddbcfd041dc
binalayjki <binalayjki@gmail.com>
2025-11-19 00:58:33 +0800

    Kassandra problem 04

--- frontend/src/components/Layout/CalendarSidebar.vue
+++ frontend/src/components/Layout/CalendarSidebar.vue
@@ -65,7 +65,7 @@ async function fetchCareerEvents() {
       .map((app) => ({
         id: app.applicationID,
         careerID: app.careerID,
-        title: app.career?.position,
+        title: app.career?.position || "Career Interview",
         date: new Date(app.interviewSchedule).toISOString().split("T")[0],
         type: "career",
         interviewSchedule: app.interviewSchedule,
