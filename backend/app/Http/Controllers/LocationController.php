6083e3cd4b62598030fbbfa73c6ac09121faef13
binalayjki <binalayjki@gmail.com>
2025-11-30 09:29:43 +0800

    Kssd org 03

--- backend/app/Http/Controllers/LocationController.php
+++ backend/app/Http/Controllers/LocationController.php
@@ -7,50 +7,47 @@ use Illuminate\Http\JsonResponse;
 
 class LocationController extends Controller
 {
-    protected function fetchJson(string $url): JsonResponse
+    public function regions(): JsonResponse
     {
-        try {
-            $response = Http::withHeaders([
-                'User-Agent' => 'MyApp/1.0' // GitHub often requires a user-agent
-            ])->get($url);
+        $res = Http::get('https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/regions.json');
 
-            if ($response->successful()) {
-                return response()->json($response->json());
-            } else {
-                return response()->json([
-                    'error' => 'Failed to fetch data',
-                    'status' => $response->status()
-                ], $response->status());
-            }
-        } catch (\Exception $e) {
-            return response()->json([
-                'error' => 'Exception fetching data',
-                'message' => $e->getMessage()
-            ], 500);
+        if ($res->failed()) {
+            return response()->json(['error' => 'Failed to fetch data', 'status' => $res->status()], 404);
         }
+
+        return response()->json($res->json());
     }
 
-    public function regions()
+    public function provinces(): JsonResponse
     {
-        $url = "https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/regions.json";
-        return $this->fetchJson($url);
+        $res = Http::get('https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/provinces.json');
+
+        if ($res->failed()) {
+            return response()->json(['error' => 'Failed to fetch data', 'status' => $res->status()], 404);
+        }
+
+        return response()->json($res->json());
     }
 
-    public function provinces()
+    public function cities(): JsonResponse
     {
-        $url = "https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/provinces.json";
-        return $this->fetchJson($url);
+        $res = Http::get('https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/cities.json');
+
+        if ($res->failed()) {
+            return response()->json(['error' => 'Failed to fetch data', 'status' => $res->status()], 404);
+        }
+
+        return response()->json($res->json());
     }
 
-    public function cities()
+    public function barangays(): JsonResponse
     {
-        $url = "https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/cities.json";
-        return $this->fetchJson($url);
-    }
+        $res = Http::get('https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/barangays.json');
 
-    public function barangays()
-    {
-        $url = "https://raw.githubusercontent.com/iamkevinluke/philippines-regions-provinces-cities-municipalities-barangays/master/barangays.json";
-        return $this->fetchJson($url);
+        if ($res->failed()) {
+            return response()->json(['error' => 'Failed to fetch data', 'status' => $res->status()], 404);
+        }
+
+        return response()->json($res->json());
     }
 }
