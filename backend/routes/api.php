b896ded7ca495171e9df1317def837f3850098bf
binalayjki <binalayjki@gmail.com>
2025-12-04 20:25:20 +0800

    delete 03

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -428,6 +428,7 @@ Route::get('/admin/applicants', [ApplicantController::class, 'index']);
 Route::delete('/admin/applicants/{id}', [ApplicantController::class, 'destroyById']);
 Route::get('/admin/organizations', [OrganizationController::class, 'index']);
 Route::delete('/admin/organizations/{id}', [OrganizationController::class, 'destroyById']);
+Route::delete('/admin/applicants/{id}', [ApplicantController::class, 'destroy']);
 
 // Admin – Organization approval system
 Route::get('/admin/pending-organizations', [OrganizationController::class, 'pending']);
